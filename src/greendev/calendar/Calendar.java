653dbcc9aad1025ab6817f89836531f6e5ba49d3
Eunyoung KANG <greenpianorabbit@localhost>
2021-08-06 03:20:27 +0900

    "println 사용해서 출력하기" 사전 과제 v1

--- src/greendev/calendar/Calendar.java
+++ src/greendev/calendar/Calendar.java
@@ -3,6 +3,13 @@ package greendev.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Calendar");
+		System.out.println("Hello, Calendar");
+		System.out.println(" 일 월  화 수  목 금 토");
+		System.out.println("--------------------");
+		System.out.println("          1  2  3  4");
+		System.out.println(" 5  6  7  8  9 10 11");
+		System.out.println("12 13 14 15 16 17 18");
+		System.out.println("19 20 21 22 23 24 25");
+		System.out.println("26 27 28 29 30 31");
 	}
 }
