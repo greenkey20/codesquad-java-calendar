64dd23b330573889b2f14af5e7802f010f15836a
Eunyoung KANG <greenpianorabbit@localhost>
2021-08-06 03:10:00 +0900

    main method 내용 추가(실수 수정)

--- src/greendev/calendar/Calendar.java
+++ src/greendev/calendar/Calendar.java
@@ -3,8 +3,6 @@ package greendev.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Calendar");
 	}
-
 }
