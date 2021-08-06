89e0db5d5f1114fd3a247ad029b19197dde44ce6
Eunyoung KANG <greenpianorabbit@localhost>
2021-08-07 01:34:45 +0900

    "두 수의 합 구하기" 따라서 해 보기 v1

--- src/greendev/calendar/Calendar.java
+++ src/greendev/calendar/Calendar.java
@@ -1,7 +1,6 @@
 package greendev.calendar;
 
 public class Calendar {
-
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
 		System.out.println(" 일 월  화 수  목 금 토");
