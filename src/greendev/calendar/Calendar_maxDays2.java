8b3b1fff49b303a1e582e241308c42eb5012b778
Eunyoung KANG <greenpianorabbit@localhost>
2021-08-07 15:07:16 +0900

    class, method 사용하여 "입력받은 월의 최대 일수 출력" 따라하기

--- src/greendev/calendar/Calendar_maxDays2.java
+++ src/greendev/calendar/Calendar_maxDays2.java
@@ -3,14 +3,20 @@ package greendev.calendar;
 import java.util.Scanner;
 
 public class Calendar_maxDays2 {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth (int month) {
+		return MAX_DAYS[month - 1];
+	}	
+	
 	public static void main(String[] args) {
-		System.out.println("달을 입력하세요");
 		Scanner scanner = new Scanner(System.in);
+		Calendar_maxDays2 cal = new Calendar_maxDays2();
+		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
+		
+		System.out.printf("%d월은 %d일까지 있습니다", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다", month, maxDays[month - 1]);
 	}
 }
